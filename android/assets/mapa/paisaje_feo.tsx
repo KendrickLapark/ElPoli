e0541c915611adcf157df3ebe796c91696d3674f
KendrickLapark <jose.exposito002a@gmail.com>
2020-02-14 20:49:43 +0100

    Probando Mapa, Personaje y movimiento de camara

---
+++ android/assets/mapa/paisaje_feo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="paisaje_feo" tilewidth="32" tileheight="16" tilecount="4500" columns="60">
+ <image source="../atlas/paisaje_feo.jpg" width="1920" height="1200"/>
+</tileset>
